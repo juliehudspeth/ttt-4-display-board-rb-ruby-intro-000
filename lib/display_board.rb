4ee65fc53d4dde33a979faa694dec0875fbaa257
Julie <juliejhudspeth@gmail.com>
2018-05-14 14:17:06 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,3 +8,33 @@ end
 
 board = [" "," "," "," "," "," "," "," "," "]
 display_board (board)
+
+board = [" "," "," "," ","X"," "," "," "," "]
+display_board (board)
+
+board = ["O"," "," "," "," "," "," "," "," "]
+display_board (board)
+
+board = ["O"," "," "," ","X"," "," "," "," "]
+display_board (board)
+
+board = ["X","X","X"," "," "," "," "," "," "]
+display_board (board)
+
+board = [" "," "," "," "," "," ","O","O","O"]
+display_board (board)
+
+board = ["X"," "," "," ","X"," "," "," ","X"]
+display_board (board)
+
+board = [" "," ","O"," ","O"," ","O"," "," "]
+display_board (board)
+
+board = [" ","X","O," "," ","X","O","X"," "]
+display_board (board)
+
+board = ["X","X","X","X","X","X","X","X","X"]
+display_board (board)
+
+board = ["O","O","O","O","O","O","O","O","O"]
+display_board (board)
